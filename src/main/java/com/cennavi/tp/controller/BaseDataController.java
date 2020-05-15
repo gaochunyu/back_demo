68082ed9b38d7bf11541d0c0257c8b9592ead9f6
maliwei <maliwei@cennavi.com.cn>
2020-05-15 17:24:27 +0800

    清理多余实体类、添加卡片信息

--- src/main/java/com/cennavi/tp/controller/BaseDataController.java
+++ src/main/java/com/cennavi/tp/controller/BaseDataController.java
@@ -1,103 +1,51 @@
 package com.cennavi.tp.controller;
 
-import com.alibaba.fastjson.JSONObject;
-import com.cennavi.tp.beans.BaseRtic;
+import com.cennavi.tp.beans.DataCard;
+import com.cennavi.tp.beans.UserinfoBean;
+import com.cennavi.tp.common.result.Result;
+import com.cennavi.tp.common.result.ResultModel;
 import com.cennavi.tp.service.BaseDataService;
-import com.cennavi.tp.utils.JsonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.*;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
- * Created by sunpengyan on 2019/9/7.
+ * 基础数据处理
  */
 @Controller
-@RequestMapping("/basedata")
+@RequestMapping("/base")
 public class BaseDataController {
 
     @Autowired
     private BaseDataService baseDataService;
 
-    /**
-     * 框选link
-     * @param linkid linkid所在道路下的所有linkid 与geometry的交集
-     * @param geometry geojson格式
-     * @return
-     */
     @ResponseBody
-    @RequestMapping("/getLinkByGeometry")
-    public String getLinkByGeometry(@RequestParam(value = "linkid",required = false) String linkid,
-                                      @RequestParam(value = "geometry",required = false) String geometry,
-                                    @RequestParam(value = "city",required = false,defaultValue = "xian") String city) {
-        JSONObject json = new JSONObject();
-        try {
-            List<Map<String,Object>> list = baseDataService.getLinkByGeometry(linkid,geometry,city);
-            json = JsonUtils.packJson(list);
-        } catch (Exception e) {
+    @RequestMapping("/getDataCard")
+    public ResultModel getDataCard(HttpServletRequest request){
+        try{
+            UserinfoBean user = (UserinfoBean)request.getSession().getAttribute("user");
+            if(user!=null){
+//                Map<>
+                if(user.getRole()==0){//超级管理员
+                    List<DataCard> list = baseDataService.getVerifyDataCard(user.getId());
+                    return Result.success("success",list);
+                }else if(user.getRole()==1){//管理员
+                    List<DataCard> list = baseDataService.getDataCard(user.getId());
+                    return Result.success("success",list);
+                }else{
+                    return Result.fail("该权限用户无信息卡片");
+                }
+            }else{
+                return Result.fail("获取用户信息失败");
+            }
+        }catch (Exception e){
             e.printStackTrace();
-            json = JsonUtils.packJsonErr(e.getMessage());
-        }finally {
-            return JsonUtils.objectToJson(json,new String[]{});
+            return Result.build500("出现异常");
         }
     }
 
-    @ResponseBody
-    @RequestMapping("/getLinkById")
-    public String getLinkById(@RequestParam(value = "id") String id,
-                              @RequestParam(value = "city",required = false,defaultValue = "xian") String city) {
-        JSONObject json = new JSONObject();
-        try {
-            List<Map<String,Object>> list = baseDataService.getLinkById(id,city);
-            json = JsonUtils.packJson(list);
-        } catch (Exception e) {
-            e.printStackTrace();
-            json = JsonUtils.packJsonErr(e.getMessage());
-        }finally {
-            return JsonUtils.objectToJson(json,new String[]{});
-        }
-    }
-
-
-    /**
-     * 框选rtic
-     * @param linkid linkid所在道路下的所有linkid 与geometry的交集
-     * @param geometry geojson格式
-     * @return
-     */
-    @ResponseBody
-    @RequestMapping("/getRticByGeometry")
-    public String getRticByGeometry(@RequestParam(value = "linkid",required = false) String linkid,
-                                    @RequestParam(value = "geometry",required = false) String geometry,
-                                    @RequestParam(value = "city",required = false,defaultValue = "xian") String city) {
-        JSONObject json = new JSONObject();
-        try {
-            List<Map<String,Object>> list = baseDataService.getRticByGeometry(linkid,geometry,city);
-            json = JsonUtils.packJson(list);
-        } catch (Exception e) {
-            e.printStackTrace();
-            json = JsonUtils.packJsonErr(e.getMessage());
-        }finally {
-            return JsonUtils.objectToJson(json,new String[]{});
-        }
-    }
-
-    @ResponseBody
-    @RequestMapping("/getRticById")
-    public String getRticById(@RequestParam(value = "id") String id,
-                              @RequestParam(value = "city",required = false,defaultValue = "xian") String city) {
-        JSONObject json = new JSONObject();
-        try {
-            List<BaseRtic> list = baseDataService.getRticById(id,city);
-            json = JsonUtils.packJson(list);
-        } catch (Exception e) {
-            e.printStackTrace();
-            json = JsonUtils.packJsonErr(e.getMessage());
-        }finally {
-            return JsonUtils.objectToJson(json,new String[]{});
-        }
-    }
 }
