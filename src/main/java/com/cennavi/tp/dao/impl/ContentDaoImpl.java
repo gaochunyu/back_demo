88b59990f465921e84cb131e2daf6a591bf1e1de
姚文帅 <792616568@qq.com>
2020-05-15 14:36:42 +0800

    1. 修改内容模块和帮助模块的sql注入的代码

--- src/main/java/com/cennavi/tp/dao/impl/ContentDaoImpl.java
+++ src/main/java/com/cennavi/tp/dao/impl/ContentDaoImpl.java
@@ -5,18 +5,10 @@ import com.cennavi.tp.dao.ContentDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.jdbc.core.PreparedStatementCreator;
-//import org.springframework.jdbc.support.GeneratedKeyHolder;
-//import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-//import java.sql.Connection;
-//import java.sql.PreparedStatement;
-//import java.sql.SQLException;
-//import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.util.List;
-import java.util.Map;
-//import java.util.Objects;
 
 
 /**
@@ -29,35 +21,51 @@ public class ContentDaoImpl extends BaseDaoImpl<ContentBean> implements ContentD
 
     public int insertItem(ContentBean contentBean){
 
-        String sql = "insert into content(id,title,sub_title,content,tags,file,create_time,uid) values ('"+contentBean.getId()+"','"+contentBean.getTitle()+"','"+contentBean.getSub_title()+"','"+contentBean.getContent()+"','"+contentBean.getTags()+"','"+contentBean.getFile()+"','"+contentBean.getCreate_time()+"',"+contentBean.getUid()+")";
-        int result = jdbcTemplate.update(sql);
+        String sql = "insert into content(id,title,sub_title,content,tags,file,create_time,uid) values (?,?,?,?,?,?,?,?)";
+        int result =  jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1,contentBean.getId());
+            ps.setString(2,contentBean.getTitle());
+            ps.setString(3, contentBean.getSub_title());
+            ps.setString(4, contentBean.getContent());
+            ps.setString(5, contentBean.getTags());
+            ps.setString(6, contentBean.getFile());
+            ps.setString(7, contentBean.getCreate_time());
+            ps.setInt(8, contentBean.getUid());
+            return ps;
+        });
+//        int result = jdbcTemplate.update(sql, new Object[]{,,,,,,,}, Integer.class);
 
         return result;
     };
 
     // 根据 id 查询某条数据
     public ContentBean getItemById(int id){
-        String sql = "select * from content where id = "+id;
-        List<ContentBean> list = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(ContentBean.class));
+        String sql = "select * from content where id = ?";
+        List<ContentBean> list = jdbcTemplate.query(sql, new Object[]{id},BeanPropertyRowMapper.newInstance(ContentBean.class));
         return list.size()==0?null:list.get(0);
     };
 
 
     // 修改某条数据
     public void updateItemById(int id, ContentBean contentBean){
-        String sql = "update content set sub_title='"+contentBean.getSub_title() +
-                "', content='"+contentBean.getContent() +
-                "', file='"+contentBean.getFile() +
-                "', tags='"+contentBean.getTags() +
-                "' where id = "+contentBean.getId();
-            jdbcTemplate.update(sql);
+        String sql = "update content set sub_title=?, content= ?,file=?,tags=? where id = ?";
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1,contentBean.getSub_title());
+            ps.setString(2,contentBean.getContent());
+            ps.setString(3, contentBean.getFile());
+            ps.setString(4, contentBean.getTags());
+            ps.setInt(5, contentBean.getId());
+            return ps;
+        });
     }
 
     @Override
     public List<ContentBean> getContentsByTags(String tags) {
         String sql = "select c.id, c.title,c.tags,c.create_time,c.sub_title,c.uid,u.username username from content c inner" +
-                " join userinfo u on c.uid = u.id inner join menu m on c.id = m.id and m.status = 2 where c.tags like '%"+tags+"%'";
-        return jdbcTemplate.query(sql,BeanPropertyRowMapper.newInstance(ContentBean.class));
+                " join userinfo u on c.uid = u.id inner join menu m on c.id = m.id and m.status = 2 where c.tags like '%?%'";
+        return jdbcTemplate.query(sql,new Object[]{tags},BeanPropertyRowMapper.newInstance(ContentBean.class));
 
     };
 
