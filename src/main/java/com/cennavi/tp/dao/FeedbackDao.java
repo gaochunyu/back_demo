c3ef2b13d2d48af06ccd82bbfc489807312b6329
huweiwei@cennavi.com.cn <huweiwei@cennavi.com.cn>
2020-05-20 17:50:58 +0800

    意见反馈模块代码

---
+++ src/main/java/com/cennavi/tp/dao/FeedbackDao.java
@@ -0,0 +1,8 @@
+package com.cennavi.tp.dao;
+
+import com.cennavi.tp.beans.FeedbackBean;
+import com.cennavi.tp.common.base.dao.BaseDao;
+
+public interface FeedbackDao extends BaseDao<FeedbackBean> {
+
+}
---
+++ src/main/java/com/cennavi/tp/dao/impl/FeedbackDaoImpl.java
@@ -0,0 +1,10 @@
+package com.cennavi.tp.dao.impl;
+
+import com.cennavi.tp.beans.FeedbackBean;
+import com.cennavi.tp.common.base.dao.impl.BaseDaoImpl;
+import com.cennavi.tp.dao.FeedbackDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FeedbackDaoImpl extends BaseDaoImpl<FeedbackBean>  implements FeedbackDao {
+}
