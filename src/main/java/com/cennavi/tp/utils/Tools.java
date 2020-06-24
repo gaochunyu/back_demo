2c9d1226628db3539b2d99d81afda4de545114b7
maliwei <maliwei@cennavi.com.cn>
2020-06-24 18:22:06 +0800

    添加用户权限管理

--- src/main/java/com/cennavi/tp/utils/Tools.java
+++ src/main/java/com/cennavi/tp/utils/Tools.java
@@ -1,5 +1,6 @@
 package com.cennavi.tp.utils;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 import java.util.HashMap;
@@ -10,16 +11,17 @@ public class Tools {
     private static String[][] models = {{"6","指南"},{"7","工具"},{"8","组件模块"},{"9","案例集锦模块"},{"10","帮助"},{"11","笔记"}};
 
     public static String getModelsMap(String ms){
-        Map<Integer,String> map = new HashMap<>();
+        Map<String,String> map = new HashMap<>();
         String[] msTmp = ms.split(",");
         for(String info : msTmp){
             for(String[] info2 : models){
                 if(info.equals(info2[0])){
-                    map.put(Integer.parseInt(info),info2[1]);
+                    map.put("id",info2[0]);
+                    map.put("name",info2[1]);
                 }
             }
         }
-        JSONObject jsonObject = JSONObject.parseObject(map.toString());
-        return jsonObject.toString();
+        String jsonString= JSON.toJSONString(map);
+        return jsonString;
     }
 }
